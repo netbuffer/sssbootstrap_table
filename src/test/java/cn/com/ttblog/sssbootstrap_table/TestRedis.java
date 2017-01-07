fa1d6ea794d6c82aabafe6d7b8039ca3a5ef19c9
netbuffer <javawiki@163.com>
2017-01-07 18:24:18 +0800

    test

--- src/test/java/cn/com/ttblog/sssbootstrap_table/TestRedis.java
+++ src/test/java/cn/com/ttblog/sssbootstrap_table/TestRedis.java
@@ -1,14 +1,16 @@
 package cn.com.ttblog.sssbootstrap_table;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.redis.connection.RedisConnection;
-import org.springframework.data.redis.core.RedisCallback;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.connection.StringRedisConnection;
+import org.springframework.data.redis.core.*;
 import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import redis.clients.jedis.Jedis;
@@ -22,6 +24,8 @@ public class TestRedis {
 
     @Resource
     private RedisTemplate redisTemplate;
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
     @Test
     public void testPing(){
@@ -35,11 +39,47 @@ public class TestRedis {
         LOG.debug("redisTemplate.keys(\"*\"):{}",redisTemplate.keys("*"));
     }
 
+    //http://www.tuicool.com/articles/3aAbMz
+    @Test
+    public void testListOperations() {
+        String key = "spring";
+        ListOperations<String, String> lop = redisTemplate.opsForList();
+        RedisSerializer<String> serializer = new StringRedisSerializer();
+        redisTemplate.setKeySerializer(serializer);
+        redisTemplate.setValueSerializer(serializer);
+        // rt.setDefaultSerializer(serializer);
+        lop.leftPush(key, "aaa");
+        lop.leftPush(key, "bbb");
+        long size = lop.size(key); // rt.boundListOps(key).size();
+        Assert.assertEquals(2, size);
+    }
+    @Test
+    public void testStringRedisTemplate() {
+        LOG.debug("stringRedisTemplate.isExposeConnection():{}",stringRedisTemplate.isExposeConnection());
+        ValueOperations<String, String> vop = stringRedisTemplate.opsForValue();
+        String key = "string_redis_template";
+        String v = "use StringRedisTemplate set k v";
+        vop.set(key, v);
+        LOG.debug("vop.size(key):{}",vop.size(key));
+        String value = vop.get(key);
+        Assert.assertEquals(v, value);
+
+        Long dbsize = (Long) stringRedisTemplate.execute(new RedisCallback<Object>() {
+            @Override
+            public Long doInRedis(RedisConnection connection) throws DataAccessException {
+                StringRedisConnection stringRedisConnection = (StringRedisConnection) connection;
+                return stringRedisConnection.dbSize();
+            }
+        });
+        LOG.debug("redis 0-数据库key数量:{}",dbsize);
+    }
+
     @Test
     public void testAdd(){
         Object result=redisTemplate.execute(new RedisCallback() {
             @Override
             public Object doInRedis(RedisConnection redisConnection) throws DataAccessException {
+                redisConnection.select(1);//选择数据库
                 RedisSerializer<String> serializer = redisTemplate.getStringSerializer();
                 byte[] key  = serializer.serialize("username");
                 byte[] name = serializer.serialize("java");
