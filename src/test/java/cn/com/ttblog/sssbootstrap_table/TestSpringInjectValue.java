ab458f311d20d664dadceb42ba8f6bc584910c6b
netbuffer <javawiki@163.com>
2017-05-12 16:14:04 +0800

    test @Value

--- src/test/java/cn/com/ttblog/sssbootstrap_table/TestSpringInjectValue.java
+++ src/test/java/cn/com/ttblog/sssbootstrap_table/TestSpringInjectValue.java
@@ -1,20 +1,14 @@
 package cn.com.ttblog.sssbootstrap_table;
 
-import cn.com.ttblog.sssbootstrap_table.model.User;
-import org.joda.time.DateTime;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.web.context.WebApplicationContext;
 
 @WebAppConfiguration
 @ContextConfiguration(locations = { "classpath:spring/spring-context.xml","classpath:spring/spring-mvc.xml" })
