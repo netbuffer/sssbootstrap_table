cac1315aa8534ace44de4b1812835fa0c77fff59
netbuffer <javawiki@163.com>
2017-05-24 14:54:23 +0800

    test Comparator

---
+++ src/main/java/cn/com/ttblog/sssbootstrap_table/model/UserCompartor.java
@@ -0,0 +1,21 @@
+package cn.com.ttblog.sssbootstrap_table.model;
+
+import java.util.Comparator;
+
+/**
+ * User类型比较器
+ */
+public class UserCompartor implements Comparator<User>{
+
+    /**
+     * 根据年龄大小来比较，可以在这里定制加入更多的比较策略
+     * @param o1
+     * @param o2
+     * @return
+     */
+    @Override
+    public int compare(User o1, User o2) {
+        return o1.getAge().compareTo(o2.getAge());
+    }
+
+}
