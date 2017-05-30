cdf23cf8c151c43166e1dcd2578f1fa7d853adad
netbuffer <javawiki@163.com>
2017-05-30 10:56:53 +0800

    fix mysql error 150

--- src/main/java/cn/com/ttblog/sssbootstrap_table/model/Team.java
+++ src/main/java/cn/com/ttblog/sssbootstrap_table/model/Team.java
@@ -4,6 +4,11 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
 
 import javax.persistence.*;
 
+/**
+ * MySQL error 150：http://www.examw.com/biancheng/mysql/165562/
+ * InnoDB引擎下teamCode为非主键列需要做唯一索引，才能在leader表中创建外键引用
+   ALTER TABLE `t_team` ADD UNIQUE INDEX `team_code_unique_index` (`teamCode`) ;
+ */
 @Entity
 @Table(name = "t_team")
 public class Team {
@@ -21,6 +26,7 @@ public class Team {
     }
 
     private String name;
+    @Column(unique = true)
     private String teamCode;
 
     public String getName() {
