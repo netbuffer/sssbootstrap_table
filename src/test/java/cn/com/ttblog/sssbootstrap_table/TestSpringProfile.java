604ff66e26c2ea6fd676e6e4b958453800611930
netbuffer <javawiki@163.com>
2017-07-06 09:42:32 +0800

    spring context util

--- src/test/java/cn/com/ttblog/sssbootstrap_table/TestSpringProfile.java
+++ src/test/java/cn/com/ttblog/sssbootstrap_table/TestSpringProfile.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
@@ -15,29 +16,36 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.web.context.WebApplicationContext;
 import cn.com.ttblog.sssbootstrap_table.model.User;
 
-@WebAppConfiguration
-@ContextConfiguration(locations = { "classpath:spring/spring-context.xml","classpath:spring/spring-mvc.xml" })
+//@WebAppConfiguration
+//@ContextConfiguration(locations = { "classpath:spring/spring-context.xml","classpath:spring/spring-mvc.xml" })
+@ContextConfiguration(locations = { "classpath:spring/spring-context.xml" })
 @RunWith(SpringJUnit4ClassRunner.class)
-@ActiveProfiles(profiles = "production")
+@ActiveProfiles(profiles = "test")
 public class TestSpringProfile {
 	private static final Logger log = LoggerFactory
 			.getLogger(TestSpringProfile.class);
 	@Autowired
-	protected WebApplicationContext wac;
+	protected ApplicationContext applicationContext;
 
 	@Before
 	public void before() {
 		log.warn("set spring.profiles.active->production");
 //		System.setProperty("spring.profiles.active", "production");
-		((GenericApplicationContext)wac).getEnvironment().setActiveProfiles("production");
+		((GenericApplicationContext)applicationContext).getEnvironment().setActiveProfiles("production");
 	}
 
 	@Test
 	public void test() {
-		log.info("getStartupDate：{}", new DateTime(wac.getStartupDate())
+		log.info("getStartupDate：{}", new DateTime(applicationContext.getStartupDate())
 				.toString("yyyy-MM-dd HH:mm:ss"));
-		log.info("getEnvironment：{}", wac.getEnvironment());
-		log.info("userFromSpring：{}", wac.getBean("userFromSpring", User.class));
+		log.info("getEnvironment：{}", applicationContext.getEnvironment());
+		log.info("getDefaultProfiles：{},getActiveProfiles:{}", applicationContext.getEnvironment().getDefaultProfiles(),applicationContext.getEnvironment().getActiveProfiles());
+		log.info("userFromSpring：{}", applicationContext.getBean("userFromSpring", User.class));
+	}
+
+	@Test
+	public void testGetEnv() {
+		log.info("System.getenv():{}",System.getenv());
 	}
 
 }
