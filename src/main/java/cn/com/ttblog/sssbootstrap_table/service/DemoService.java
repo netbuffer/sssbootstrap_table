5b1f4f8e174e80db0fc5c7b7cea572fea9002f6d
netbuffer <javawiki@163.com>
2018-01-25 14:22:02 +0800

    SSE(Server sent events)

--- src/main/java/cn/com/ttblog/sssbootstrap_table/service/DemoService.java
+++ src/main/java/cn/com/ttblog/sssbootstrap_table/service/DemoService.java
@@ -1,12 +1,16 @@
 package cn.com.ttblog.sssbootstrap_table.service;
 
+import cn.com.ttblog.sssbootstrap_table.constant.ConfigConstant;
 import cn.com.ttblog.sssbootstrap_table.exception.CustomGenericException;
+import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Service;
 import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.util.concurrent.TimeUnit;
 
@@ -31,5 +35,19 @@ public class DemoService {
             e.printStackTrace();
         }
         return new AsyncResult<Object>(param);
+    }
+
+    @Async
+    public void setSSEData(SseEmitter sseEmitter) {
+        try {
+            for (int i = 0; i < 10; i++) {
+                sseEmitter.send(DateTime.now().toString(ConfigConstant.DATE_LONG_FORMAT), MediaType.TEXT_PLAIN);
+                TimeUnit.SECONDS.sleep(1);
+            }
+        } catch (Exception e) {
+            sseEmitter.completeWithError(e);
+            LOGGER.info("error:{}", e.getMessage());
+        }
+        sseEmitter.complete();
     }
 }
